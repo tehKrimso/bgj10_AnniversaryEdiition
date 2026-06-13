d87607f33316747da5e45bcc3215419e57e0a49b
Tim Shuvalov <tashuvalov@gmail.com>
2026-06-13 23:42:27 +0300

    powerfull target ability

--- Assets/Scripts/Behaviours/Towers/PowerfulTower.cs
+++ Assets/Scripts/Behaviours/Towers/PowerfulTower.cs
@@ -9,30 +9,49 @@ namespace Behaviours
         public float DoubleTargetTime;
         private BaseEnemy _mainTarget;
         private BaseEnemy _secondaryTarget;
+
+        private bool _doubleTargetOn;
+        private float _doubleTargetTimer;
         
         
         private void Update()
         {
             base.Update();
+            if(_doubleTargetOn)
+                CheckDoubleTarget();
         }
         
         public override void PerformBasicAttack()
         {
-            if (_mainTarget == null)
+            //perf?
+            if (_mainTarget == null || (_secondaryTarget == null && _doubleTargetOn))
             {
                 var targets = targetFinder.GetPotentialTargets();
-                _mainTarget = targets.OrderBy(t => Vector3.Distance(t.transform.position, _centerTilePosition)).FirstOrDefault();
+                var targetsByDistanceToCore = targets.OrderBy(t => Vector3.Distance(t.transform.position, _centerTilePosition)).ToList();
+                _mainTarget = targetsByDistanceToCore[0];
+                _secondaryTarget = targetsByDistanceToCore[1];
                 
             }
             
             _mainTarget?.TakeDamage(parameters.Damage);
+            _secondaryTarget?.TakeDamage(parameters.Damage);
             
             Debug.Log("PowerfulTowerAttack");
         }
 
         public override void PerformAbility()
         {
-            throw new System.NotImplementedException();
+            _doubleTargetTimer = DoubleTargetTime;
+            _doubleTargetOn = true;
+        }
+
+        private void CheckDoubleTarget()
+        {
+            _doubleTargetTimer -= Time.deltaTime;
+            if (_doubleTargetTimer <= 0)
+            {
+                _doubleTargetOn = false;
+            }
         }
     }
 }
